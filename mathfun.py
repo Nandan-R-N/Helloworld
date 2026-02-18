197378825d3966006a81be4f007740d01ce97b9d
Nandan-R-N <nrnayaka2003@gmail.com>
2026-02-18 18:26:28 +0530

    Add files via upload

---
+++ mathfun.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+arr1 = np.array([10, 20, 30, 40])
+arr2 = np.array([1, 2, 3, 4])
+
+print("Array 1:", arr1)
+print("Array 2:", arr2)
+
+
+print("Addition:", arr1 + arr2)
+
+print("Subtraction:", arr1 - arr2)
+
+print("Multiplication:", arr1 * arr2)
+
+print("Division:", arr1 / arr2)
