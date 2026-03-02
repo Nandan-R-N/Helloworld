6f41fef016c7a39124fd3e77a69bef2a0b8d9935
Nandan-R-N <nrnayaka2003@gmail.com>
2026-03-02 12:04:04 +0530

    Add files via upload

---
+++ NumpyArrays.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# Create NumPy arrays
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([5, 6, 7, 8])
+
+# Display arrays
+print("Array 1:", arr1)
+print("Array 2:", arr2)
+
+# Addition
+addition = arr1 + arr2
+print("Addition:", addition)
+
+# Multiplication
+multiplication = arr1 * arr2
+print("Multiplication:", multiplication)
